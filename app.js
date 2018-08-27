55cd1bb3db1e7b9a94e1c01ee63d6e5878f31ee2
Laraib anwar <laraib.anwar919@gmail.com>
2018-08-27 22:34:27 +0530

    initial commit

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ mongoose.Promise = Promise;
 
 
 
-//mongoose.connect("mongodb://laraib:laraib.anwara1@ds147461.mlab.com:47461/shopkart");
-mongoose.connect("mongodb://localhost/shopping_cart");
+mongoose.connect("mongodb://laraib:laraib.anwara1@ds147461.mlab.com:47461/shopkart");
+//mongoose.connect("mongodb://localhost/shopping_cart");
 //require('./config/passport')(passport);
 
 
